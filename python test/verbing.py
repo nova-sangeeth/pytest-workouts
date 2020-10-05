5459a2a7a12ffa0e6afe6a77f5d8086235376e24
nova-sangeeth <novasangeeth@outlook.com>
2020-10-05 13:33:13 +0530

    removed the commented out code

--- python test/verbing.py
+++ python test/verbing.py
@@ -1,14 +1,3 @@
-# a = "roting"
-
-
-# def verbing(a):
-#     if len(a) < 3 and a[-3:] == "ing":
-#         return a + "ly"
-#     else:
-#         return a + "ing"
-
-
-# print(verbing(a))
 a = "roting"
 
 
