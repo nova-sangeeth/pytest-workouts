9bb2535dee143b2f1fb514a97760f3e91840ec6d
nova-sangeeth <novasangeeth@outlook.com>
2020-10-06 00:38:54 +0530

    finished front back

---
+++ python test/front_back.py
@@ -0,0 +1,46 @@
+a = "kitten"
+b = "donut"
+
+
+# def front_back(a, b):
+#     if len(a) and len(b) % 2 == 0:
+
+#         print(a[0:3] + " " + b[-3:])
+#         print("this one is even")
+
+#     else:
+
+#         print("Add another to the front.")
+
+
+# print(front_back(a, b))
+
+
+# def front_back(a, b):
+#     if len(a) and len(b) % 2 == 0:
+#         first_part
+#         second_part
+#         third_part
+#         fourth_part
+
+
+# def front_back(a, b):
+#     len_a, len_b = (len(a) + 1) / 2, (len(b) + 1) / 2
+#     return a[:len_a] + b[:len_b] + a[len_a:] + b[len_b:]
+
+
+# print(front_back(a, b))
+
+
+# METHOD n
+
+
+def front_back(a, b):
+    a_string = len(a) / 2 + (len(a) % 2)
+    b_string = len(b) / 2 + (len(b) % 2)
+
+    return a[:a_string] + b[:b_string] + a[a_string:] + b[b_string:]
+
+
+print(front_back(a, b))
+
