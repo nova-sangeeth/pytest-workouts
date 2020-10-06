bfe8da343ee6f75bd63c8f4736e21fc4c2b6266b
nova-sangeeth <novasangeeth@outlook.com>
2020-10-06 17:32:36 +0530

    fixed verbing

--- python test/verbing.py
+++ python test/verbing.py
@@ -1,12 +1,37 @@
-a = "roting"
+# a = "do"
 
 
-def verbing(a):
-    if len(a) > 3 and a[-3:] == "ing":
-        a += "ly"
-    else:
-        a += "ing"
-    return a
+# def verbing(a):
+#     if len(a) >= 3 and a[-3:] == "ing":
+#         a += "ly"
+#     else:
+#         a += "ing"
+#     return a
 
 
-print(verbing(a))
+# print(verbing(a))
+
+
+# a = "runing"
+
+
+# def verbing(a):
+#     if len(a) <= 3:
+#         return a
+#     elif a[-3] == "ing":
+#         a += "ly"
+#     else:
+#         a += "ing"
+#     return a
+
+
+# print(verbing(a))
+
+
+def verbing(s):
+    if len(s) >= 2:
+        if s[-3:] == "ing":
+            s += "ly"
+        else:
+            s += "ing"
+        return s
