1aa5e10afee264b4ac2e2e8031c5b3b17242b085
nova-sangeeth <novasangeeth@outlook.com>
2020-10-06 17:27:03 +0530

    front_x finished

--- python test/front_back.py
+++ python test/front_back.py
@@ -1,5 +1,5 @@
-a = "kitten"
-b = "donut"
+a = "abcde"
+b = "xyz"
 
 
 # def front_back(a, b):
@@ -24,23 +24,24 @@ b = "donut"
 #         fourth_part
 
 
-# def front_back(a, b):
-#     len_a, len_b = (len(a) + 1) / 2, (len(b) + 1) / 2
-#     return a[:len_a] + b[:len_b] + a[len_a:] + b[len_b:]
+# added // for the division part cuz this one outputs a float
+def front_back(a, b):
+    len_a, len_b = (len(a) + 1) // 2, (len(b) + 1) // 2
+    return a[:len_a] + b[:len_b] + a[len_a:] + b[len_b:]
 
 
-# print(front_back(a, b))
+print(front_back(a, b))
 
 
 # METHOD n
 
 
-def front_back(a, b):
-    a_string = len(a) / 2 + (len(a) % 2)
-    b_string = len(b) / 2 + (len(b) % 2)
+# def front_back(a, b):
+#     a_string = len(a) / 2 + (len(a) % 2)
+#     b_string = len(b) / 2 + (len(b) % 2)
 
-    return a[:a_string] + b[:b_string] + a[a_string:] + b[b_string:]
+#     return a[:a_string] + b[:b_string] + a[a_string:] + b[b_string:]
 
 
-print(front_back(a, b))
+# print(front_back(a, b))
 
