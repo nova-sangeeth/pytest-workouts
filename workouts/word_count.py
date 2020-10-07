354f60bd19d16fd2dd149d1021e6ab1defdf6bae
nova-sangeeth <novasangeeth@outlook.com>
2020-10-07 20:01:47 +0530

    commented out the old code that was used for reference.

--- workouts/word_count.py
+++ workouts/word_count.py
@@ -1,3 +1,24 @@
+# print("the wordcount program")
+
+
+# text = open("/home/nova/webdev-lessons/python-test/workouts/file..txt", "r")
+
+# d = dict()
+# # lower casing the whole file and sorting it accordingly
+# # def word_count(d, text):
+# for line in text:
+#     line = line.strip()
+#     line = line.lower()
+#     words = line.split(" ")
+#     for word in words:
+#         if word in d:
+#             d[word] = d[word] + 1
+#         else:
+#             d[word] = 1
+
+
+# for key in list(d.keys()):
+#     print(key, ":", d[key])
 import sys
 
 # from .
