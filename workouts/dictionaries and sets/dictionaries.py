526dfa5b1122add164502ce695118d2eeded1d53
nova-sangeeth <novasangeeth@outlook.com>
2020-10-08 00:23:41 +0530

    worked out dictionaries.

---
+++ workouts/dictionaries and sets/dictionaries.py
@@ -0,0 +1,17 @@
+print("dictionaries and adding data into the dictionaries with keypair values.")
+
+dict = {}
+
+
+dict["a"] = "first"
+dict["b"] = "second"
+dict["c"] = "third"
+dict["d"] = "fourth"
+dict["e"] = "fifth"
+dict["f"] = "sixth"
+
+print(dict)
+
+print(dict["b"])
+print(dict["e"])
+
