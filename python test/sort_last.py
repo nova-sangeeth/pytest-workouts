47011a0998b1799cd4ff019303767dbf8135f64f
nova-sangeeth <novasangeeth@outlook.com>
2020-10-07 13:35:15 +0530

    fixed the errors in sort last.

--- python test/sort_last.py
+++ python test/sort_last.py
@@ -1,6 +1,13 @@
 tuples = [(1, 7), (1, 3), (3, 4, 5), (2, 2)]
 
 
+def last(t):
+    return t[-1]
+
+
 def sort_last(tuples):
-    return
+    return sorted(tuples, key=last)
+
+
+print(sort_last(tuples))
 
