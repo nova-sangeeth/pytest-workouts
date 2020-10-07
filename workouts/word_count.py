b690538c710567daa394467fc16a8999df50a1b0
nova-sangeeth <novasangeeth@outlook.com>
2020-10-07 20:03:46 +0530

    worked out the first method to check the program weather it is working or not.

--- workouts/word_count.py
+++ workouts/word_count.py
@@ -21,43 +21,29 @@
 #     print(key, ":", d[key])
 import sys
 
-# from .
+print("The word count Program 2")
+
+
 def sort_value(item):
     return item[-1]
 
 
-file_name = "text_needed.txt"
+filename = "/home/nova/webdev-lessons/python-test/workouts/file..txt"
 
 
-def build_dict(file_name):
-    f = open(file_name, "rU")
+def build_dict(filename):
+    f = open(filename, "rU")
     words = f.read().split()
     count = {}
-
     for word in words:
         word = word.lower()
         if word not in count:
             count[word] = 1
         else:
             count[word] += 1
-
     f.close()
 
     return count
 
 
-def print_words(file_name):
-    dict = build_dict(file_name)
-
-    for word in sorted(dict.keys()):
-        print(word, dict[word])
-
-
-def print_top(file_name):
-    count = build_dict(file_name)
-    i = 0
-
-    items = sorted(count.items(), key=sort_value, reverse=True)
-    for item in items[:20]:
-        print(item[0] + str(item[1]) + "times")
-        i += 1
+print(build_dict(filename))
