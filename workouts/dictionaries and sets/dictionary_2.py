7775dc64c6bd019277fb168ca8294d2d35fe2966
nova-sangeeth <novasangeeth@outlook.com>
2020-10-09 00:07:15 +0530

    workouts with dictionaries again.

---
+++ workouts/dictionaries and sets/dictionary_2.py
@@ -0,0 +1,22 @@
+print("new dictionary")
+
+dict = {}
+
+
+dict["a"] = "hello"
+dict["b"] = "There"
+dict["c"] = "how"
+dict["d"] = "are"
+dict["e"] = "you"
+
+
+print(dict)
+
+
+dict["a"] = "Hey"
+
+print(dict)
+
+
+print(dict.get("c"))
+
