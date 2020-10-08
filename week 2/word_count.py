7124be7dea73278fcf32d99183d56a3a3a19f6b5
nova-sangeeth <novasangeeth@outlook.com>
2020-10-08 15:50:52 +0530

    added the context manager for the file. with the path.

--- week 2/word_count.py
+++ week 2/word_count.py
@@ -33,30 +33,57 @@ import sys
 # You could write a helper utility function that reads a file
 # and builds and returns a word/count dict for it.
 # Then print_words() and print_top() can just call the utility function.
-def sort_value(item):
-    return item[-1]
 
 
 ###
+# filename = "/home/nova/webdev-lessons/python-test/week 2/text_needed.txt"
+
+f = open("/home/nova/webdev-lessons/python-test/week 2/text_needed.txt", "r+")
+
+
+def build_dict(f):
+    # f = open(f, "r+")
+    words = f.read().split()
+
+    count = {}
+    for word in words:
+        word = word.lower()
+        if word not in count:
+            count[word] = 1
+        else:
+            count[word] += 1
+        f.close()
+
+    return count
+
+
+def print_words(f):
+    word_count = build_dict(f)
+    words = sorted(word_count.keys())
+    for word in words:
+        print(word, word_count[word])
+
+
+print_words(f)
 
 # This basic command line argument parsing code is provided and
 # calls the print_words() and print_top() functions which you must define.
-def main():
-    if len(sys.argv) != 3:
-        print("usage: ./wordcount.py {--count | --topcount} file")
-        sys.exit(1)
+# def main():
+#     if len(sys.argv) != 3:
+#         print("usage: ./wordcount.py {--count | --topcount} file")
+#         sys.exit(1)
 
-    option = sys.argv[1]
-    filename = sys.argv[2]
-    if option == "--count":
-        print_words(filename)
-    elif option == "--topcount":
-        print_top(filename)
-    else:
-        print("unknown option: " + option)
-        sys.exit(1)
+#     option = sys.argv[1]
+#     filename = sys.argv[2]
+#     if option == "--count":
+#         print_words(filename)
+#     elif option == "--topcount":
+#         print_top(filename)
+#     else:
+#         print("unknown option: " + option)
+#         sys.exit(1)
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
 
