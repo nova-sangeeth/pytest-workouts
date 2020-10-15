22903bdc1d5c306f659bf1f2926fca8cff45db89
nova-sangeeth <novasangeeth@outlook.com>
2020-10-15 14:14:40 +0530

    printing the basic list index.

---
+++ workouts/dictionaries and sets/lists.py
@@ -0,0 +1,10 @@
+print("list")
+# printing the colors as per its index values
+colors = ["red", "blue", "green", "black", "violet", "yellow", "cyan"]
+
+print(colors[3])
+print(colors[5])
+print(colors[2])
+
+print(colors[1])
+
