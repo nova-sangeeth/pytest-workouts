d99149af973e1b08156f2f3f7aed58112ec93cc0
nova-sangeeth <novasangeeth@outlook.com>
2020-10-16 23:51:39 +0530

    added the same logic in a function

--- workouts/regex/email_regex.py
+++ workouts/regex/email_regex.py
@@ -1,7 +1,17 @@
 import re
 
-email = 'dog dog-qewrtyg@ggy.com'
+email = 'dog dog-qewrtyg@ggy.com' 
 
 match = re.search(r'\w+@\w+' , email)
 if match: 
     print(match.group())
+
+email_2 = "main good helloworld@gmail.com ee"
+
+def reg_main(email_2):
+    match = re.search(r'\w+@\w+', email_2)
+    if match:
+        print(match.group())
+
+
+reg_main(email_2)    
